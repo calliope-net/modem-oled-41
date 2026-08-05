641d438bd840e921dc80d4aa2b13f8281a53305a
calliope-net <141074597+calliope-net@users.noreply.github.com>
2026-08-05 13:51:09 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 function senden (send_text: string) {
     modem.comment("blau: Text senden")
+    i2c_lock = true
     if (led_an) {
         led_an = false
         pins.pinDigitalWrite(modem.get_settings(modem.e_settings.pin_led), led_an)
         basic.pause(2000)
     }
-    ft_messen = false
     anzeige01_aktualisieren()
     basic.setLedColor(0x0000ff)
     pins.oled_clear(2, 4)
@@ -20,11 +20,14 @@ function senden (send_text: string) {
     modem.sende_code(13)
     pins.oled_write_text(2, 0, 15, "Senden Ende " + send_text.length)
     led_aktualisieren()
+    i2c_lock = false
 }
 function GitHub () {
     modem.comment("calliope-net/modem-41")
     modem.comment("2 Erweiterungen laden:")
     modem.comment("calliope-net/modem; calliope-net/pins")
+    modem.comment("Lichtschranken-LED; Fototransistor")
+    modem.comment("I²C OLED; optional Keypad oder Keyboard")
 }
 pins.onKeyboardEvent(function (zeichenCode, zeichenText, isASCII) {
     if (s_text.length == 0) {
@@ -68,12 +71,11 @@ function led_aktualisieren () {
     }
 }
 input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
-    modem.empfang_abbrechen()
-    empf_break = true
+	
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     modem.comment("rot: Text empfangen")
-    ft_messen = false
+    i2c_lock = true
     anzeige01_aktualisieren()
     basic.setLedColor(0xff0000)
     empf_break = false
@@ -83,15 +85,13 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     pins.oled_write_text(7, 0, 15, e_text)
     pins.oled_write_text(5, 0, 15, "Empfang Ende " + e_text.length)
     led_aktualisieren()
+    i2c_lock = true
 })
 function anzeige01_aktualisieren () {
+    basic.pause(100)
     pins.oled_write_text(0, 0, 15, "hell<" + modem.get_settings(modem.e_settings.helligkeit) + "<dunkel")
     pins.oled_write_text(1, 0, 6, "FT " + pins.pinAnalogRead(modem.get_settings(modem.e_settings.pin_fototransistor)))
-    if (ft_messen) {
-        pins.oled_write_text(1, 7, 15, pins.pins_text("dauerhaft"))
-    } else {
-        pins.oled_write_text(1, 7, 15, "Takt " + modem.get_settings(modem.e_settings.takt_ms))
-    }
+    pins.oled_write_text(1, 7, 15, "Takt " + modem.get_settings(modem.e_settings.takt_ms))
 }
 input.onButtonEvent(Button.A, ButtonEvent.Hold, function () {
     modem.comment("LED dauerhaft an/aus schalten")
@@ -101,37 +101,34 @@ input.onButtonEvent(Button.A, ButtonEvent.Hold, function () {
     pins.oled_write_text(3, 0, 15, "Senden LED " + led_an)
 })
 input.onButtonEvent(Button.B, ButtonEvent.Hold, function () {
-    modem.comment("dauerhaft Helligkeit messen")
-    ft_messen = !(ft_messen)
-    anzeige01_aktualisieren()
+    modem.empfang_abbrechen()
+    empf_break = true
 })
 let e_text = ""
 let empf_asc = 0
 let empf_break = false
 let empf_text = ""
 let s_text = ""
-let ft_messen = false
 let led_an = false
+let i2c_lock = false
 let kb_on = false
 if (!(pins.simulator())) {
-    modem.set_pins(DigitalPin.C17, AnalogPin.C16, 15)
+    modem.set_pins(DigitalPin.C17, AnalogPin.C16)
     modem.set_takt(50, 0.5, 1)
-    led_aktualisieren()
     pins.oled_reset(pins.oled_pages.y64, false, true)
     anzeige01_aktualisieren()
-    kb_on = pins.buffer_getUint8(pins.pins_i2cReadBuffer(pins.pins_i2cAdressen(pins.ei2cAdressen.CardKB_x5F), 1), 0) == 13
+    kb_on = pins.keyboard_read() == 13
     if (kb_on) {
         pins.oled_write_text(2, 0, 15, pins.pins_text("Keyboard on"))
     } else if (pins.keypadConnected()) {
         pins.oled_write_text(2, 0, 15, pins.pins_text("Keypad on"))
     }
+    led_aktualisieren()
 }
 loops.everyInterval(500, function () {
-    pins.raiseKeyboardEvent(kb_on)
-    pins.raiseKeypadEvent(pins.keypadConnected())
-})
-loops.everyInterval(500, function () {
-    if (ft_messen) {
+    if (!(i2c_lock)) {
         pins.oled_write_text(1, 0, 6, "FT " + pins.pinAnalogRead(modem.get_settings(modem.e_settings.pin_fototransistor)))
+        pins.raiseKeyboardEvent(kb_on)
+        pins.raiseKeypadEvent(pins.keypadConnected())
     }
 })
